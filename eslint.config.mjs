e8172fa4f0586b6114a8dff7e6dd07c36f70a229
Untest57 <khseok87n@gmail.com>
2024-09-18 18:30:08 +0900

    chore: eslint & stylelint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,25 @@ export default [
   {
     ...html.configs['flat/recommended'],
     files: ['**/*.html'],
+    rules: {
+      ...html.configs['flat/recommended'].rules,
+      // prettier 충돌
+      '@html-eslint/no-extra-spacing-attrs': [
+        'error',
+        {
+          enforceBeforeSelfClose: true,
+        },
+      ],
+      '@html-eslint/require-closing-tags': [
+        'error',
+        {
+          selfClosing: 'always',
+        },
+      ],
+      '@html-eslint/indent': 'off',
+      '@html-eslint/attrs-newline': 'off',
+      '@html-eslint/element-newline': 'off',
+    },
   },
   eslintConfigPrettier,
 ];
--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('stylelint').Config} */
 export default {
-  extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recess-order',
+    'stylelint-config-html/html',
+  ],
 };
