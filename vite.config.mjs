4995082ae21387816e3c3cc2db47026e24878e16
Untest57 <khseok87n@gmail.com>
2024-09-19 19:47:34 +0900

    chore: bun, /node_module

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,18 @@
-import { extname, relative } from 'path';
+import { extname, relative, resolve } from 'path';
 import { fileURLToPath } from 'node:url';
 import FastGlob from 'fast-glob';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src',
+  appType: 'mpa',
+  root: 'src/',
+  resolve: {
+    alias: {
+      // node_modules 폴더를 절대 경로로 설정
+      '/node_modules': resolve(import.meta.dirname, 'node_modules'),
+    },
+  },
   build: {
-    outDir: '../dist',
     emptyOutDir: true,
     rollupOptions: {
       input: Object.fromEntries(
