caa2c29887f2b5b422dc90efe7c3f611cada3963
Untest57 <khseok87n@gmail.com>
2024-10-09 22:07:10 +0900

    [07-testimonials-grid-section] END

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -3,8 +3,18 @@ export default {
   extends: [
     'stylelint-config-standard-scss',
     'stylelint-config-html/html',
-    'stylelint-config-two-dash-bem',
     'stylelint-config-clean-order',
     'stylelint-config-clean-order/error',
+    'stylelint-config-prettier-scss',
   ],
+  rules: {
+    'selector-class-pattern': [
+      '^([a-z]+:)?[a-z][-a-z0-9]+(__[-a-z0-9]+)?(--[a-z0-9]+)?$',
+      {
+        message: (selector) =>
+          `Selector class ${selector} violates BEM Convention`,
+        resolveNestedSelectors: true,
+      },
+    ],
+  },
 };
